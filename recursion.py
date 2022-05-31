3f98abf0b3f5c1a70c620608092199a271a41bcb
JimWasHere <98727234+JimWasHere@users.noreply.github.com>
2022-05-30 19:27:47 -0500

    Added a couple of functions

--- recursion.py
+++ recursion.py
@@ -37,4 +37,62 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-print(fibonacci(60))
+# print(fibonacci(60))
+
+# Sum of Digits
+
+def sum_of_digits(n):
+    assert n >= 0 and int(n) == n, "Must be greater than zero and a real number"
+    if n == 0:
+        return 0
+    else:
+        return int(n % 10) + sum_of_digits(int(n//10))
+
+
+# print(sum_of_digits(6543))
+
+# calculate power using recursion
+
+def power(base, exponent):
+    assert exponent >= 0 and int(exponent) == exponent, "Exponent cannot be negative and must be a whole number"
+    if exponent == 0:
+        return 1
+    if exponent == 1:
+        return base
+    else:
+        return base * power(base, exponent - 1)
+
+
+# print(power(2, 4))
+
+# Greatest common divisor
+
+def greatest_common_divisor(n1, n2):
+    assert int(n1) == n1 and int(n2) == n2, "Numbers must be integers"
+    if n1 < 0:
+        n1 = abs(n1)
+    if n2 < 0:
+        n2 = abs(n2)
+    while n2 == 0:
+        return n1
+    else:
+        return greatest_common_divisor(n2, n1 % n2)
+
+# print(greatest_common_divisor(48052, -18492))
+
+
+# Decimal to binary
+# Divide number by 2
+# get integer quotient for next iteration
+# get remainder for binary digit
+# repeat until quotient is 0
+
+def d2b(n):
+    assert n == int(n), "Must be integer"
+    if n == 0:
+        return 0
+    else:
+        return n % 2 + 10 * d2b(int(n / 2))
+
+
+print(d2b(10034))
